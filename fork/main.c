6aeb509d33c61c625bfc786b461dfde8fff17e15
Frank <gzhuflyer@foxmail.com>
2020-12-03 23:11:27 +0800

    .

--- fork/main.c
+++ fork/main.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
 
-int main()
+
+void test()
 {
+	
+}
 
+int main(int argc, char *argv[])
+{
+	pid_t pid = fork();
+	if(pid < 0)
+	{
+		fprintf(stderr,"创建进程出错\n");
+	}else if(pid == 0){
+		while(1){
+			printf("子进程空间,g_count=%d\n",g_count++);
+			sleep(3);
+		}	
+	}else{
+		while(1){
+			printf("父进程空间，子进程pid为%d,g_coutn=%d\n",pid,g_count++);
+			sleep(1);	
+		}
+	}
 	return 0;
 }
