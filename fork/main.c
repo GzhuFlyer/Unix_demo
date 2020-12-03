21a4fda1c388f5252845254d8c1ae7d1b1f34435
gzhu_frank <gzhuflyer@foxmail.com>
2020-12-04 00:16:50 +0800

    .

--- fork/main.c
+++ fork/main.c
@@ -3,27 +3,26 @@
 #include<stdlib.h>
 
 
-void test()
-{
-	
-}
-
 int main(int argc, char *argv[])
 {
-	pid_t pid = fork();
-	if(pid < 0)
+	for(int i=0;i<4;i++)	//进行4次fork, 进程数为16,打印数为：2+4+8+16=30
 	{
-		fprintf(stderr,"创建进程出错\n");
-	}else if(pid == 0){
-		while(1){
-			printf("子进程空间,g_count=%d\n",g_count++);
-			sleep(3);
-		}	
-	}else{
-		while(1){
-			printf("父进程空间，子进程pid为%d,g_coutn=%d\n",pid,g_count++);
-			sleep(1);	
+		pid_t pid = fork();
+		if(pid < 0)
+		{
+			fprintf(stderr,"创建进程出错\n");
+		}else if(pid == 0){
+			//while(1){
+				printf("子进程空间,getppid()=%d\n",getpid());
+				//sleep(3);
+			//}	
+		}else{
+			//while(1){
+				printf("父进程空间，子进程pid为%d,getppid()=%d\n",pid,getpid());
+				//sleep(1);	
+			//}
 		}
 	}
+
 	return 0;
 }
---
+++ multiServer/server.cc
@@ -0,0 +1,84 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#define ERR_EXIT(M) \
+        do  \
+        {   \
+            perror(M);  \
+            exit(EXIT_FAILURE); \
+        }while(0)
+void do_service(int conn)
+{
+    char recvbuf[1024];
+    while(1)
+    {
+        memset(recvbuf, 0, sizeof(recvbuf));
+        int ret = read(conn,recvbuf,sizeof(recvbuf));
+        if(ret == 0)    //当返回0表示客户端关闭了
+        {
+            printf("client close\n");
+            break;//退出函数，关闭该通信
+        }else
+        {
+            ERR_EXIT("read");
+        }
+        
+        fputs(recvbuf,stdout);
+        write(conn,recvbuf,sizeof(recvbuf));
+    }   
+}
+int main(void)
+{
+    int listenfd;
+    if((listenfd = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP)) < 0)
+        ERR_EXIT("socket");
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = PF_INET;
+    servaddr.sin_port = htons(5188);
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    //服务器重新启动处于 TIME_WAIT 状态
+    //使用REUSEADDR选项可以使得不必等待TIME_WAIT状态消失就可以重启服务器
+    int on = 1;
+    if(setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on)) < 0)
+        ERR_EXIT("setsockopt");
+    if(bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
+        ERR_EXIT("bind");
+    if(listen(listenfd,SOMAXCONN) < 0)
+        ERR_EXIT("listen");
+    struct sockaddr_in peeraddr;
+    socklen_t peerlen = sizeof(peeraddr);
+    int conn;
+    pid_t pid;
+    while(1)
+    {
+        if((conn = accept(listenfd,(struct sockaddr*)&peeraddr,&peerlen)) < 0)
+            ERR_EXIT("accept");
+        //将ip地址转换成点分十进制的ip地址
+        printf("ip=%s,port=%d\n",inet_ntoa(peeraddr.sin_addr),ntohs(peeraddr.sin_port));
+        //当接受到一个连接，fork一个进程
+        pid = fork();   //多进程
+        if(pid == -1)
+            ERR_EXIT("fork");
+        if(pid == 0)   //子进程 
+        {   
+            //子进程不需要处理监听，将监听套接口关闭
+            close(listenfd);
+            do_service(conn);
+            exit(EXIT_SUCCESS);//退出子进程
+        }else
+        {
+            //父进程不需要处理连接，关闭连接套接口
+            close(conn);
+        }
+    }
+
+    return 0;
+}       
